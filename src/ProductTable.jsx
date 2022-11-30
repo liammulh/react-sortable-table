eee93a0be5923d36e6388cc63a81fe00a0906809
Liam Mulhall <liammulh@gmail.com>
2022-11-30 11:38:29 -0700

    feat: add functionality to `ProductTable`

--- src/ProductTable.jsx
+++ src/ProductTable.jsx
@@ -1,26 +1,58 @@
-function ProductTable( props ) {
-  const { products } = props;
+import useSortableData from './useSortableData.jsx';
+
+const ProductTable = ( props ) => {
+  const { items, requestSort, sortConfig } = useSortableData( props.products );
+  const getClassNamesFor = ( name ) => {
+    if ( !sortConfig ) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
   return (
     <table>
-      <caption>Our products</caption>
+      <caption>Products</caption>
       <thead>
       <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th>In Stock</th>
+        <th>
+          <button
+            type="button"
+            onClick={() => requestSort( 'name' )}
+            className={getClassNamesFor( 'name' )}
+          >
+            Name
+          </button>
+        </th>
+        <th>
+          <button
+            type="button"
+            onClick={() => requestSort( 'price' )}
+            className={getClassNamesFor( 'price' )}
+          >
+            Price
+          </button>
+        </th>
+        <th>
+          <button
+            type="button"
+            onClick={() => requestSort( 'stock' )}
+            className={getClassNamesFor( 'stock' )}
+          >
+            In Stock
+          </button>
+        </th>
       </tr>
       </thead>
       <tbody>
-      {products.map( product => (
-        <tr key={product.id}>
-          <td>{product.name}</td>
-          <td>{product.price}</td>
-          <td>{product.stock}</td>
+      {items.map( ( item ) => (
+        <tr key={item.id}>
+          <td>{item.name}</td>
+          <td>${item.price}</td>
+          <td>{item.stock}</td>
         </tr>
       ) )}
       </tbody>
     </table>
   );
-}
+};
 
 export default ProductTable;
